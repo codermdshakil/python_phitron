8c90d781e890fb314af2adf14f5758eb2ed6d53f
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-23 23:26:36 +0600

    Learn : variable and command

---
+++ week-1/Module-1/variable.py
@@ -0,0 +1,35 @@
+# String
+name = "Shakil Ahmed"
+district = "Gazipur"
+
+# Integer
+age = 19
+
+# Float
+c_point = 3.57
+
+# Boolean
+is_single = True
+
+
+# None
+poket = None
+
+# Print results
+
+print(type(age)) # int
+
+print(name)
+print(age + c_point)
+print(is_single)
+print(district)
+print(poket)
+
+## Commands
+# There are two way to do command using hash
+# Ctrl + / : # : Single line command 
+# Alt + shift + A  -> """  """ : multiline command
+
+
+
+
