2d84e57f97fc5ad35bd51dae1d52ab815515334b
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-23 23:54:33 +0600

    Learn : Input and typecasting

---
+++ week-1/Module-1/input.py
@@ -0,0 +1,72 @@
+
+# print("Now I need money")
+# input()
+
+# money = input("Give me some money : ")
+# print("Here is your money :  " ,money)
+
+# first_money = input("Kodon ali, Dosto kisu taka ke : ")
+# second_money = input("Peyara begum, dosto kisu taka de : ")
+
+# by default input type is string
+# total_money = first_money + second_money
+
+
+#typecasting
+# first_money_int = int(first_money)
+# second_money_int = int(second_money)
+
+# total = first_money_int + second_money_int;
+# print(total)
+
+""" Goole Search
+-----------------
+User input convert
+Typecasting
+
+1. convert number to string : str
+2. convert decimal number : float
+3. python int vs float
+
+"""
+
+# Practice 
+
+#Convert string to number
+#--------------------------
+# a = "10"
+# b = "20"
+# r = a + b # 1020
+
+# a_int = int(a)
+# b_int = int(b)
+# result1 = a_int + b_int
+# print(result1) # 30
+
+
+# Convert number to string
+#--------------------------
+# a = str(10)
+# b = str(20)
+# r = a + b
+# print(r)
+
+# Convert Int to float
+
+# a = int(10)
+# b = int(20.5)
+
+# a_float = float(a)
+# b_float = float(b)
+
+# r = a_float + b_float
+# print(r)
+
+
+
+
+
+
+
+
+
