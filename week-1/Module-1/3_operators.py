ddb5cd9f283cc79fecb540c06b53ecf00ab700ea
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-24 21:41:01 +0600

    Learn : Arithmetic Operators

---
+++ week-1/Module-1/3_operators.py
@@ -0,0 +1,13 @@
+# Arithmetic operators
+
+num1 = 45
+num2 = 5
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2) # Floor Division
+print(num1 % num2) 
+print( 3 ** 5)
+
+
