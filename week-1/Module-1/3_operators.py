72a3ea603867f9c483d3bf62de3727ef6079348b
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-24 21:52:16 +0600

    Learn : Membership Operators

--- week-1/Module-1/3_operators.py
+++ week-1/Module-1/3_operators.py
@@ -27,9 +27,35 @@ y = 3
 a = True
 b = False
 
-print(a and b) # if one is false then result is false
-print(a or b) # if one is true then result if true
-print(not a) # make positive ti negative
+# print(a and b) # if one is false then result is false
+# print(a or b) # if one is true then result if true
+# print(not a) # make positive ti negative
+
+
+# Assignment Operators
+
+# Assignemnt Operators 
+# += 
+# -+
+# *=
+# /=
+# **=
+# //= 
+x = 5
+# x += 3
+# or 
+x = x + 3
+# print(x)
+
+# Membership Operator
+
+lists = [1,2,3]
+
+print(2 in lists) # True
+print(5 in lists) # False
+print(7 not in lists) # True
+
+
 
 
 
