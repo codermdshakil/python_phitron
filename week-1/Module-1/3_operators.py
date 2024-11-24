2c99bb4ae7ec2321750e3ee465ecc524b09ccfea
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-24 21:42:57 +0600

    Learn : Comparison Operator

--- week-1/Module-1/3_operators.py
+++ week-1/Module-1/3_operators.py
@@ -10,4 +10,18 @@ print(num1 // num2) # Floor Division
 print(num1 % num2) 
 print( 3 ** 5)
 
+# Comparison Operators
+
+x = 10
+y = 3
+
+print(x > y)  # Greater than
+print(x < y)  # Less than
+print(x == y) # Equal to
+print(x != y) # Not equal to
+print(x >= y) # Greater than or equal to
+print(x <= y) # Less than or equal to
+
+
+
 
