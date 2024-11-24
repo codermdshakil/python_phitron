2cc791d7f238313d5edf010535e4163eeb521eea
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-24 21:45:56 +0600

    Learn : Logical Operators

--- week-1/Module-1/3_operators.py
+++ week-1/Module-1/3_operators.py
@@ -2,26 +2,34 @@
 
 num1 = 45
 num2 = 5
-print(num1 + num2)
-print(num1 - num2)
-print(num1 * num2)
-print(num1 / num2)
-print(num1 // num2) # Floor Division
-print(num1 % num2) 
-print( 3 ** 5)
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 // num2) # Floor Division
+# print(num1 % num2) 
+# print( 3 ** 5)
 
 # Comparison Operators
 
 x = 10
 y = 3
 
-print(x > y)  # Greater than
-print(x < y)  # Less than
-print(x == y) # Equal to
-print(x != y) # Not equal to
-print(x >= y) # Greater than or equal to
-print(x <= y) # Less than or equal to
+# print(x > y)  # Greater than
+# print(x < y)  # Less than
+# print(x == y) # Equal to
+# print(x != y) # Not equal to
+# print(x >= y) # Greater than or equal to
+# print(x <= y) # Less than or equal to
 
+# Logical Operators
+
+a = True
+b = False
+
+print(a and b) # if one is false then result is false
+print(a or b) # if one is true then result if true
+print(not a) # make positive ti negative
 
 
 
