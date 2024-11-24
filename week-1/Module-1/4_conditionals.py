75de02b2d089051357531417a50b1f0dcb5f0c73
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-24 22:30:12 +0600

    Learn : Conditionals of python , if, elif, else, nested if_else

---
+++ week-1/Module-1/4_conditionals.py
@@ -0,0 +1,35 @@
+
+# x= 3
+# if x > 5:
+#     print("X is greater than 5")
+# else:
+#     print("X is 5 or less")
+
+
+# x = 20
+# if x < 10:
+#     print("x is less then 10")
+#     print(f"X value is {x}")
+# elif x < 20:
+#     print("X value is between 10 and 20")
+#     print(f"X value is {x}")
+
+# else:
+#     print("x value is 20 or more")
+#     print(f"X value is {x}")
+
+
+# x = 4
+# if x >= 5:
+#     if x < 10:
+#         print("x is between 5 and 10")
+#     else:
+#         print("X is greater then 10 or more then 10")
+# else:
+#     print("less then 5")
+
+
+x = 7
+print("x is greater than 5") if x > 5 else print("x is 5 or less")
+
+
