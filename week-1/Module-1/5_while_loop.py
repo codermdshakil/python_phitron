d143e6a2e36914743be2e50c62ce773984d189b7
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-25 21:46:26 +0600

    Learn : While Loop

--- week-1/Module-1/5_while_loop.py
+++ week-1/Module-1/5_while_loop.py
@@ -1,8 +1,41 @@
 
+# num = 1
+# While Loop
+#------------
+
+# while num < 10:
+#     print(num)
+#     num += 1
+
+## Break and Continue
+
+# num = 1
+# while num < 10:
+#     print(num)
+#     num += 1
+#     if num == 5:
+#         break
+
+
+# num = 1
+# while num < 10:
+#     num += 1
+#     if num == 5:
+#         continue # continue will skip value
+#     print(num)
+
+
+## Continue is skip values 
+
 num = 1
 
-while num < 10:
+while num <= 10:
+    num = num + 1
+    if num % 2 == 1:
+        continue
     print(num)
-    num += 1
+
+    
+   
     
 
