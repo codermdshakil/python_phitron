ac907f88c19891fbc772eb5c1c6d9e05546a1d89
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-25 21:56:42 +0600

    Learn : For Loop, Range For Loop

---
+++ week-1/Module-1/6_for_loop.py
@@ -0,0 +1,33 @@
+
+numbers = [5, 10, 15,20,25]
+
+# Using for loop iterate array
+# for num in numbers:
+#     print(num)
+
+
+# sum = 0
+# for num in numbers:
+#     sum += num;
+#     print(num)
+#     if sum > 20:
+#         print("Bigger sum", sum)
+# print(sum)
+
+# text = "Shakil Ahmed"
+
+# for char in text:
+#     print(char)
+    
+
+
+## Range based For loop
+
+# for i in range(1,11):
+#     print(i)
+
+
+# step increase
+for i in range(1,11, 2):
+    print(i)
+    
