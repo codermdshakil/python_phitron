5b0d847c05c692f5fddc84948c4de6d2073977b5
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-26 23:32:23 +0600

    Learn : Function of python

---
+++ week-1/Module-2/function.py
@@ -0,0 +1,30 @@
+
+
+#define
+
+def double_it (num):
+    result = num * 2
+    print(result)
+    return result
+    
+# call double it function
+
+double_it(5)
+double_it(8)
+
+
+def sum(num1, num2):
+    result = num1 + num2
+    return result;
+
+total = sum(10, 50)
+print( 'Total value is ', total)
+
+final = double_it(total)
+print("Final value", final)
+
+
+
+
+
+
