efb981a1669431991e25778f0b26a2ac6ba0906b
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-28 21:35:35 +0600

    Learn : build in function of python

---
+++ week-1/Module-2/5_build_in_function.py
@@ -0,0 +1,13 @@
+
+highest = max(45,23,24,56,67,35,98,46)
+highest2 = max([45,23,24,56,67,35,98,46])
+minimum = min(45,23,24,56,67,35,98,46)
+
+total = sum([45,23,24,56,67,35,98,46])
+count = len([45,3,5,6,3]) # len - how many element have in array
+
+print(highest, minimum, count, total)
+
+
+
+
