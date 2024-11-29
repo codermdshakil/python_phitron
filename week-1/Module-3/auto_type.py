3506623d11b1aaa395b98338cb0ec752d127b301
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-30 01:21:35 +0600

    Learn : Python auto sent message using pyautogui

---
+++ week-1/Module-3/auto_type.py
@@ -0,0 +1,11 @@
+
+import pyautogui
+from time import sleep
+
+print("Hello world")
+sleep(5)
+for i in range(0,10):
+    pyautogui.write("I love Programming and Now Python!", interval=0.25) 
+    pyautogui.press('enter')
+
+ 
