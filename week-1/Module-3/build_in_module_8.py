1e74d3cf6babc4931e875cd76c2a4cc42dd473bc
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-30 00:54:04 +0600

    Learn : Build in module in python

---
+++ week-1/Module-3/build_in_module_8.py
@@ -0,0 +1,34 @@
+
+from math import *
+from random import *
+from time import sleep
+
+r1 = ceil(4.1)
+r2 = floor(4.6)
+
+# print(r1)
+# print(r2)
+
+## get random number
+random_num = floor(random() * 100) # 0 to 100
+# print(random_num)
+print(randint(1, 100))
+
+## for Quiz
+# sleep(4)
+print(choice(['sakib', 'mosh', 'kalam', 'shakil', 'noyon', 'nadim']))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
