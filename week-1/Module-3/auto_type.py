c8c88c697ab6888a3f498990a6192a15a86afc1a
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-30 01:48:58 +0600

    Learn : exception of python

--- week-1/Module-3/auto_type.py
+++ week-1/Module-3/auto_type.py
@@ -3,10 +3,10 @@ import pyautogui
 from time import sleep
 
 print("Hello world")
-sleep(5)
-for i in range(0,10):
-    pyautogui.write("I love Programming and Now Python!", interval=0.25) 
-    pyautogui.press('enter')
+# sleep(5)
+# for i in range(0,10):
+#     pyautogui.write("I love Programming and Now Python!", interval=0.25) 
+#     pyautogui.press('enter')
 
  
 # pip install pyautogui
---
+++ week-1/Module-3/exception.py
@@ -0,0 +1,10 @@
+
+try:
+    # result = 45/0
+    result = 45/5
+except:
+    print("Error happend")
+finally:
+    print("Finally here ")
+
+print('Done')
