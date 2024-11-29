c69dff5bca97604c0c38a8b456aa184f918d424f
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-29 21:10:54 +0600

    Learn : String in python

---
+++ week-1/Module-3/string_1.py
@@ -0,0 +1,31 @@
+
+## String
+# string  is immutable so that we can't change it's value
+
+name = 'Sakib\'s Khan'
+name1 = "Sakib Khab"
+
+# multiline string
+name2 = """
+My name is sakib khan. I am Software Engineer. I love to build new things and technology
+"""
+# print(name2)
+# print(name)
+# print(name[2:])
+
+# for char in name:
+#     print(char)
+
+
+if 'khan' in name2:
+    print("YES")
+else:
+    print("NO")
+    
+    
+
+
+
+
+
+
