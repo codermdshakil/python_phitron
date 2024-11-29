9f26f9748a1cb453c94034cdf3a41f5b3c6d2fb2
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-30 01:53:33 +0600

    Learn : file read, write access

---
+++ week-1/Module-3/file.py
@@ -0,0 +1,18 @@
+# .csv comma separated value
+# .txt text file
+
+
+
+# with open('messsage.txt', 'w') as file:
+#     file.write("I love you, Python!")
+    
+# with open('messsage.txt', 'a') as file:
+#     file.write("I love you, Python!")
+    
+    
+with open('messsage.txt', 'r') as file:
+     text = file.read()
+     print(text)
+     
+    
+    
