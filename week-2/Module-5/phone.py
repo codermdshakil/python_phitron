65f6376757191df5f265dcf60df7b0e798e6417d
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-03 23:01:49 +0600

    Learn : Class that store one elements attributes

---
+++ week-2/Module-5/phone.py
@@ -0,0 +1,14 @@
+
+
+class Phone:
+    princ = 20000
+    model = 'A15'
+    color = 'blue'
+    brand = 'sansung'
+    
+my_phone = Phone()
+
+print(my_phone.brand)
+print(my_phone.color)
+print(my_phone.princ)
+
