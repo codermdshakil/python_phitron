39ab3f014620005b4c732e6ce3b3eaac3bc7181d
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-07 21:54:22 +0600

    Learn : Object with Feet management system

---
+++ week-2/Module-6/feet_management.py
@@ -0,0 +1,18 @@
+
+# Ena Poribohon
+
+
+
+
+class Company:
+    
+    def __init__(self, name, address):
+        self.name = name
+        self.bus = []
+        self.routes = []
+        self.drivers = []
+        self.counters =[]
+        self.manager = []
+        self.supervisors = []
+        
+        
