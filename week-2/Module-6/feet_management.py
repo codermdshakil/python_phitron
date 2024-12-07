1a6f37ff1f190b19b67b234c1cf16a0ee165a58b
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-07 22:03:29 +0600

    Learn : Objects

--- week-2/Module-6/feet_management.py
+++ week-2/Module-6/feet_management.py
@@ -14,5 +14,29 @@ class Company:
         self.counters =[]
         self.manager = []
         self.supervisors = []
+        self.fare = []
         
         
+class Driver:
+    
+    def __init__(self,name, age ,license):
+        self.name = name
+        self.license = license
+        self.age = age
+        
+        
+    
+class Counter:
+    
+    def __init__(self):
+        pass
+    def purchase_ticket(self, start, destionation):
+        pass
+    
+class Passenger:
+    
+    pass
+
+red_mia = Driver('Red Mia','123',32)
+
+
