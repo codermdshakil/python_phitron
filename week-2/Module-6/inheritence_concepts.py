189ce4ac58b3fd1aace1d6b24c3b53f4ec32606d
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-08 23:56:50 +0600

    Learn : Inheritence Concepts of OOP

--- week-2/Module-6/inheritence_concepts.py
+++ week-2/Module-6/inheritence_concepts.py
@@ -1,3 +1,22 @@
 
 # Based Class Or parent class
 
+class BaseClass:
+    pass
+
+# Derived class or Children class
+class DerivedClass:
+    pass
+
+
+
+
+"""
+Relations of Inheritence 
+
+1. Single Inheritence : parent class --> child class (gadget --> Phone) (Gadgets --> phone)
+2. Multileve Inheritence : Grandpa --> Parent -> Child (Vehical --> Bus --> ACBuss)
+
+"""
+
+
