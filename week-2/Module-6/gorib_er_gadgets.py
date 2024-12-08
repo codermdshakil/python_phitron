92ec69a6d3de85bbd638084fc86ecdd9440de3c9
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-08 22:58:57 +0600

    Learn : Objects and class

--- week-2/Module-6/gorib_er_gadgets.py
+++ week-2/Module-6/gorib_er_gadgets.py
@@ -13,4 +13,44 @@ class laptop:
     def run(self):
         return f"Runnint Laptop : {self.brand}"
     
+    def coding(self):
+        return f"Learning python and practicing"
+
+
+class Phone:
+    
+    def __init__(self, brand, price, color,dual_sim):
+        
+        self.brand = brand
+        self.price = price
+        self.color = color
+        self.dual_sim = dual_sim
+        
+    
+    def run(self):
+        
+        return f"Phone tipa tipi kore"
+        
+    
+    def call(self, number, sms):        
+        return f"Sending sms to : {number} with {sms}"
+    
+    
+    
+
+class Camera:
+    
+    def __init__(self, brand, price, color, pixel):
+        self.brand = brand
+        self.price = price
+        self.color = color
+        self.pixel = pixel
+        
+    def run(self):
+        pass
+    
+    def change_lens(self):
+        pass
+        
+    
     
