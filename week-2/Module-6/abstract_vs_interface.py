1eb961434fa21cdbcb3e0e58f86da57475c966db
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-10 23:49:07 +0600

    Learn : Interface vs Abstract for interview prep

---
+++ week-2/Module-6/abstract_vs_interface.py
@@ -0,0 +1,8 @@
+
+# Interface
+# how to access value. can only have final static variables, An interface can never change it's own state
+
+# Abstract 
+# Can have any kind of instance or static variable, mutable or immutable
+
+
