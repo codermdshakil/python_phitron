6c9d154746aba711a0b286c5da32b15e9d1a8fc9
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-12 00:07:59 +0600

    Learn : Called inner function and  how to received function as a parameter

--- week-2/Module-7/inner_function_and_wrapped_func.py
+++ week-2/Module-7/inner_function_and_wrapped_func.py
@@ -10,6 +10,27 @@ def outer_function():
     inner_function()
     return 200           
 
-print(outer_function())
+# print(outer_function())
+
+
+# Functon received as a parameter and print that function
+def do_somthing(work):
+    print("work started")
+    # print(work)
+    work() 
+    print("work ended")
+
+# do_somthing('Coding with python')
+
+def coding():
+    print("Coding coding and coding and logic is important")
+    
+
+# do_somthing(coding)
+
+def sleeping():
+    print("Sleeping and dreaming with python")
+    
+do_somthing(sleeping)
 
         
