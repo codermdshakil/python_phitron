7f6c90b9e4ecd982ef57a439a862376e8cbd6eef
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-12 12:03:38 +0600

    Learn : Inheritance

---
+++ week-2/Module-7/inheritance.py
@@ -0,0 +1,18 @@
+
+#  Inheritance "is a" relation
+
+class Animal:
+    pass
+
+# Dog is a Animal
+class Dog(Animal):
+    pass
+
+# Cat is a Animal
+class Cat(Animal):
+    pass
+
+# Tiger is a Animal
+class Tiger(Animal):
+    pass
+
