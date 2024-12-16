0308e42320cb74b4201512c706fc61d1c590e581
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-17 00:08:06 +0600

    Find magnet Unpaid Customers

---
+++ week-3/Module-10_school_management/magnet_december_unpaid_awrakhali.py
@@ -0,0 +1,53 @@
+# Defining the lists
+listA = [
+    "kawsar01", "kawsar03", "kawsar04", "kawsar05", "kawsar06", "kawsar07", "kawsar08", "kawsar09", "kawsar10", 
+    "kawsar100", "kawsar101", "kawsar102", "kawsar103", "kawsar104", "kawsar105", "kawsar106", "kawsar108", 
+    "kawsar109", "kawsar11", "kawsar110", "kawsar111", "kawsar112", "kawsar114", "kawsar115", "kawsar116", 
+    "kawsar117", "kawsar118", "kawsar119", "kawsar12", "kawsar120", "kawsar121", "kawsar122", "kawsar123", 
+    "kawsar124", "kawsar125", "kawsar126", "kawsar127", "kawsar128", "kawsar129", "kawsar13", "kawsar130", 
+    "kawsar131", "kawsar132", "kawsar133", "kawsar134", "kawsar135", "kawsar136", "kawsar137", "kawsar138", 
+    "kawsar139", "kawsar140", "kawsar141", "kawsar142", "kawsar143", "kawsar144", "kawsar145", "kawsar146", 
+    "kawsar147", "kawsar148", "kawsar149", "kawsar150", "kawsar151", "kawsar152", "kawsar153", "kawsar154", 
+    "kawsar156", "kawsar157", "kawsar158", "kawsar159", "kawsar16", "kawsar160", "kawsar161", "kawsar162", 
+    "kawsar163", "kawsar164", "kawsar165", "kawsar166", "kawsar167", "kawsar168", "kawsar17", "kawsar170", 
+    "kawsar171", "kawsar173", "kawsar174", "kawsar175", "kawsar176", "kawsar18", "kawsar180", "kawsar181", 
+    "kawsar182", "kawsar183", "kawsar184", "kawsar185", "kawsar187", "kawsar188", "kawsar189", "kawsar19", 
+    "kawsar190", "kawsar191", "kawsar192", "kawsar194", "kawsar195", "kawsar196", "kawsar197", "kawsar198", 
+    "kawsar199", "kawsar20", "kawsar200", "kawsar201", "kawsar203", "kawsar204", "kawsar205", "kawsar206", 
+    "kawsar207", "kawsar21", "kawsar211", "kawsar212", "kawsar22", "kawsar23", "kawsar24", "kawsar26", 
+    "kawsar27", "kawsar28", "kawsar28fhp", "kawsar29", "kawsar30", "kawsar31", "kawsar32", "kawsar33", 
+    "kawsar35", "kawsar36", "kawsar37", "kawsar38", "kawsar39", "kawsar41", "kawsar42", "kawsar43", "kawsar44", 
+    "kawsar45", "kawsar46", "kawsar47", "kawsar48", "kawsar49", "kawsar50", "kawsar51", "kawsar52", "kawsar53", 
+    "kawsar54", "kawsar55", "kawsar57", "kawsar59", "kawsar60", "kawsar61", "kawsar62", "kawsar63", "kawsar64", 
+    "kawsar65", "kawsar66", "kawsar67", "kawsar68", "kawsar69", "kawsar70", "kawsar71", "kawsar72", "kawsar74", 
+    "Kawsar75", "kawsar76", "kawsar77", "kawsar78", "kawsar79", "kawsar80", "kawsar81", "kawsar82", "kawsar83", 
+    "kawsar84", "kawsar85", "kawsar86", "kawsar87", "kawsar89", "kawsar90", "kawsar92", "kawsar93", "kawsar94", 
+    "kawsar95", "kawsar96", "kawsar97", "kawsar98", "kawsar99", "kawser25"
+]
+
+listB = [
+    "kawsar5", "kawsar3", "kawsar71", "kawsar93", "kawsar75", "kawsar190", "kawsar110", "kawsar131", "kawsar137", 
+    "kawsar101", "kawsar167", "kawsar166", "kawsar182", "kawsar45", "kawsar130", "kawsar61", "kawsar201", 
+    "kawsar06", "kawsar67", "kawsar212", "kawsar94", "kawsar10", "kawsar161", "kawsar61", "kawsar111", "kawsar47", 
+    "kawsar48", "kawsar149", "kawsar46", "kawsar39", "kawsar55", "kawsar145", "kawsar141", "kawsar65", "kawsar65", 
+    "kawsar142", "kawsar146", "kawsar134", "kawsar41", "kawsar50", "kawsar38", "kawsar37", "kawsar159", "kawsar158", 
+    "kawsar49", "kawsar191", "kawsar126", "kawsar85", "kawsar89", "kawsar125", "kawsar92", "kawsar124", "kawsar86", 
+    "kawsar102", "kawsar109", "kawsar70", "kawsar04", "kawsar31", "kawsar204", "kawsar123", "kawsar183", 
+    "kawsar198", "kawsar35", "kawsar59", "kawsar171", "kawsar118", "kawsar187", "kawsar12", "kawsar144", 
+    "kawsar18", "kawsar79", "kawsar138", "kawsar121", "kawsar175", "kawsar16", "kawsar30", "kawsar188", 
+    "kawsar203", "kawsar68", "kawsar119", "kawsar184", "kawsar194", "kawsar52", "kawsar115", "kawsar157", 
+    "kawsar33", "kawsar44", "kawsar143", "kawsar106", "kawsar125", "kawsar180", "kawsar92", "kawsar160", 
+    "kawsar192", "kawsar53", "kawsar139", "kawsar07", "kawsar196", "kawsar100", "kawsar117", "kawsar98", 
+    "kawsar128", "kawsar08", "kawsar64", "kawsar99", "kawsar164", "kawsar72", "kawsar66"
+]
+
+
+# # Finding unique elements in each list
+unique_in_listA = set(listA) - set(listB)
+ 
+unique_in_listA 
+print(unique_in_listA)
+
+
+
+
