d281fb361129bdef7ab7dd4e4c02b9362089031a
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-16 20:38:48 +0600

    Create : Student Object

--- week-3/Module-10_school_management/person.py
+++ week-3/Module-10_school_management/person.py
@@ -1,6 +1,6 @@
 
 import random
-
+from school import School
 class Person:
     def __init__(self,name):
         self.name = name
@@ -15,5 +15,37 @@ class Teacher(Person):
         return  random.randint(1, 100)
     
     
+class Student(Person):
     
+    def __init__(self, name, classroom):
+        super().__init__(name)
+        self.classroom = classroom
+        self.__id = None
+        self.marks = {} #{'english' : 78, 'ICT': 90}
+        self.subject_grade = {} #{"eng" : 'A'}
+        self.grade = None
+        
+        
+    def final_grade(self):
+        
+        sum = 0
+        for grade in self.subject_grade.values():
+            point = School.grade_to_value(grade) # 5.00
+            sum += point
+        
+        gpa = sum / len(self.subject_grade) # 7 / 2 = 3.50
+        self.grade = School.all_value_to_grade(gpa)
+         
+    
+    # Getter VS Setter  
+    @property
+    def id(self):
+        return self.__id
+    
+    @id.setter
+    def id(self, value):
+        self.__id = value
+            
+        
+        avg = sum
     
