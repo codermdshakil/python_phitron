ef6c4ed725e822f94ed583fae51bee36afd95d6e
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-16 20:14:57 +0600

    Learn : Calculate_grade for calculate grade

--- week-3/Module-10_school_management/school.py
+++ week-3/Module-10_school_management/school.py
@@ -20,4 +20,24 @@ class School:
     def student_addmission(self, student):
         pass
     
+    
+    @staticmethod
+    
+    def calculate_grade(self,marks):
+        
+        if marks >= 80 and marks <= 100:
+            return 'A+'
+        elif marks >= 70 and marks < 80:
+            return 'A'
+        elif marks >= 60 and marks < 70:
+            return 'A-'
+        elif marks >= 50 and marks < 60:
+            return 'B'
+        elif marks >= 40 and marks < 50:
+            return 'C'
+        elif marks >= 33 and marks < 40:
+            return 'D'
+        else:
+            return "Fail" 
+        
         
