b44a5169303d834353a48b422c122d8da8725d7b
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-17 22:02:43 +0600

    Create : Library object and addBook, addUser Methods

--- week-3/Module-10.5/library.py
+++ week-3/Module-10.5/library.py
@@ -14,4 +14,37 @@ class User:
         self.bbooks = []
         self.rbooks = []
         
+# create Library 
+class Library:
+    
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+        self.users = []
+    
+    # add book method
+    def addBook(self, id, name, quantity):
+        
+        for book in self.books:
+            if book.id == id:
+                print(f"This  Book  {id} already Exist!!")
+            else:
+                 # create book using Book Object
+                book = Book(id, name, quantity)
+                self.books.append(book)
+                print(f"{book.name} Added!")
+            
+    
+    # add user method    
+    def addUser(self, id, name,password):
+        
+         for user in self.users:
+            if user.id == id:
+                print(f"This User {id} already Exist!!")
+            else:
+                 # create user using User Object
+                user = User(id, name, password)
+                self.users.append(user)
+                print(f"{user.name} Added!")
+        
         
