71d797d5815902cb32299341f7e83161cd0b3682
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-17 21:42:40 +0600

    Create : Book and User objects

---
+++ week-3/Module-10.5/library.py
@@ -0,0 +1,16 @@
+class Book:
+    
+    def __init__(self, id, name, quantity):
+        self.id = id
+        self.name = name
+        self.quantity = quantity
+
+class User:
+    
+    def __init__(self, id,name, password):
+        self.id = id 
+        self.name = name
+        self.password = password
+        
+        
+        
