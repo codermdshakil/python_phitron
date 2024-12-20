7d4f4b42b9578aac1f76d6126e125617cbf4d6dd
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-20 19:08:36 +0600

    Create : Rider Object

--- week-3/Module-11-Ride-Sharing/user.py
+++ week-3/Module-11-Ride-Sharing/user.py
@@ -12,7 +12,34 @@ class User(ABC):
     @abstractmethod
     def display_profile(self):
         raise NotImplementedError 
-    
 
+# create rider class
+
+class Rider(User):
+    
+    def __init__(self, name, email, nid, current_location, inisial_amount):
+        super().__init__(name, email, nid, inisial_amount)
+        self.current_ride = None
+        self.wallet = inisial_amount
+        self.current_location = current_location
+        
+    def display_profile(self):
+        print(F"Rider : {self.name} and email {self.email}")
+        
+    
+    # add cash to wallet
+    def load_cash(self, amount):
+        
+        if amount > 0:
+            self.wallet += amount
+        else:
+            print("Amount less than 0")
+    
+    
+    # update location
+    
+    def update_location(self, current_location):
+        self.current_location =  current_location
+    
         
         
