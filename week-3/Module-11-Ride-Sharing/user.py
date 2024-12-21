e57044a2ce8a2432fd58bf7d282f32afd5068635
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-21 10:06:26 +0600

    Create : Ride Matching Class

--- week-3/Module-11-Ride-Sharing/user.py
+++ week-3/Module-11-Ride-Sharing/user.py
@@ -62,9 +62,8 @@ class Driver(User):
         print(f"Driver Name : {self.name}")
         
     def accept_ride(self, ride):
-        # accept korbo
-        pass
-    
+        ride.set_driver(self) # Driver Object (self) mean Driver Object
+        
     
     
     
