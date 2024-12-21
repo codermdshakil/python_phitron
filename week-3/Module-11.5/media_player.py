f40669fb38e429934f862008b4d3b13ce4e5a17c
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-21 20:49:04 +0600

    Create : Music object and show info of music

--- week-3/Module-11.5/media_player.py
+++ week-3/Module-11.5/media_player.py
@@ -3,8 +3,11 @@ from abc import ABC, abstractmethod
 
 class Description:
     
-    def __init__(self):
-        self.__description
+    def __init__(self, description):
+        self.__description = description
+        
+    def get_description(self):
+        return self.__description
         
 
 class Media(ABC):
@@ -12,7 +15,6 @@ class Media(ABC):
     def __init__(self, title, duration):
         self.title = title
         self.duration = duration
-        super().__init__()
         
         
     @abstractmethod
@@ -20,9 +22,25 @@ class Media(ABC):
         pass
 
 
-class Mesic(Media, Description):
+class Music(Media, Description):
     
     def __init__(self, title, duration, description):
         Media.__init__(self, title, duration)
-        Description.__init__(description)
+        Description.__init__(self,description)
+    
+    def play(self):
+        print(f"Playing Music title : {self.title}")
+    
+    # show details of music
+    def info(self):
+        print(f"Title of music : {self.title}")
+        print(f"Duration of music : {self.duration}")
+        print(f"Description of music : {self.get_description()}")
+    
         
+        
+music1 = Music("Music1", "3.45", "This is a music and it's duration is 3.45 min")
+# music1.play()
+# music1.get_description()
+music1.info()
+
